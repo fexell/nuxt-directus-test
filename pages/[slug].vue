e7e0fd57d4e88e182ddc672e1cf5df0ae3ac70b6
FelixLocal <fexellofficial@gmail.com>
2024-10-11 19:30:15 +0200

    Removed something

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -4,8 +4,6 @@
 
   const { data } = await useAsyncData(route.params.slug, () => {
     return $directus.request($readItem('pages', route.params.slug))
-  }, {
-    transform: (data) => data[ 0 ]
   })
 
   console.log(data)
